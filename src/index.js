15c04e1a2b17151265d9160a6d6403a3bd62d67c
Ya_Myn <ricky.month@gmail.com>
2020-02-10 03:25:59 +0200

    build(yarn, contact.js, index.js): write function for a work with file, and added terminal command

--- src/index.js
+++ src/index.js
@@ -1,18 +1,49 @@
-'use strict';
+const argv = require('yargs').argv;
+const contactsAp = require('./contacts');
 
-const http = require('http');
+// contactsAp.listContacts();
+// contactsAp.getContactById(2);
+// contactsAp.removeContact(3);
+// contactsAp.addContact('Yarik', 'ya@gmail.com', '9379992');
 
-const hostname = '127.0.0.1';
 
-const port = 4000;
+function invokeAction({
+    action, id, name, email, phone
+}) {
+    switch (action) {
+    case 'list':
+        contactsAp.listContacts();
+        break;
 
-const server = http.createServer((req, res) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('Hello, World!\n');
-});
+    case 'get':
+        contactsAp.getContactById(id);
+        break;
 
-server.listen(port, hostname, () => {
-    // eslint-disable-next-line no-console
-    console.log(`Server running at http://${hostname}:${port}/`);
-});
+    case 'add':
+        contactsAp.addContact(name, email, phone);
+        break;
+
+    case 'remove':
+        contactsAp.removeContact(id);
+        break;
+
+    default:
+        console.warn('\x1B[31m Unknown action type!');
+    }
+}
+
+invokeAction(argv);
+// const hostname = '127.0.0.1';
+
+// const port = 4000;
+
+// const server = http.createServer((req, res) => {
+//     res.statusCode = 200;
+//     res.setHeader('Content-Type', 'text/plain');
+//     res.end('Hello, World!\n');
+// });
+
+// server.listen(port, hostname, () => {
+//     // eslint-disable-next-line no-console
+//     console.log(`Server running at http://${hostname}:${port}/`);
+// });
